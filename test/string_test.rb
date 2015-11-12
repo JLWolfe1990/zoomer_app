763287457653636f09adb3a59b2cc55a854388bd
Joshua Wolfe <jlwolfe1990@gmail.com>
2015-11-12 14:59:02 -0600

    Add other tests for future development

--- test/string_test.rb
+++ test/string_test.rb
@@ -5,4 +5,14 @@ class StringTest < MiniTest::Test
   def test_that_it_should_reverse_the_string
     assert_equal "cat black big the", "the big black cat".reverse_sentence
   end
+
+  def test_it_should_handle_an_empty_string
+    assert_equal "", "".reverse_sentence
+  end
+
+  # def test_it_should_ignore_punctuation
+  # end
+  #
+  # def test_it_should_capitalize_the_first_letter
+  # end
 end
